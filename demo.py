5ac6257e4abf513a5dd74f707afe9904927b69ae
Zgluckydog <2505979936@qq.com>
2025-03-21 19:50:26 +0800

    pycharm first commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+print("1")
+
+# pycharm + git 结合
+# 1. 配置git，让pycharm能使用git
+# 2. 配置github账号
+# 3. 初始化仓库，相当于git init
+# 4. 配置忽略文件 .gitignore
+# 5. 添加文件到暂存区，相当于git add
+# 6. 添加文件本地仓库，相当于git commit -m "xxx"
+# 7. 推送文件到远程仓库，相当于git push xxx
